e3e6dd687f40ae8836a5da6f996bd1e2fe05d304
Jan Tytgat <jan@timehole.be>
2020-06-12 17:41:22 +0200

    Final dev-0.0.1

--- src/CoreLayer.Citrix.Adc.NodeBackup/CoreLayer.Citrix.Adc.NodeBackupWorker/Configuration/PrometheusConfiguration.cs
+++ src/CoreLayer.Citrix.Adc.NodeBackup/CoreLayer.Citrix.Adc.NodeBackupWorker/Configuration/PrometheusConfiguration.cs
@@ -2,7 +2,7 @@ namespace CoreLayer.Citrix.Adc.NodeBackupWorker.Configuration
 {
     public class PrometheusConfiguration
     {
-        public bool Enabled { get; set; } = false;
+        public bool EnableMetricsServer { get; set; } = false;
         public string NamePrefix { get; set; } = "corelayer";
     }
 }
--- src/CoreLayer.Citrix.Adc.NodeBackup/CoreLayer.Citrix.Adc.NodeBackupWorker/NodeBackupHostedService.cs
+++ src/CoreLayer.Citrix.Adc.NodeBackup/CoreLayer.Citrix.Adc.NodeBackupWorker/NodeBackupHostedService.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 using System.Timers;
 using CoreLayer.Citrix.Adc.NitroClient;
 using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.Login;
-using CoreLayer.Citrix.Adc.NitroClient.Api.Configuration.System.SystemBackup;
-using CoreLayer.Citrix.Adc.NitroClient.Commands.Configuration.System.SystemBackup;
 using CoreLayer.Citrix.Adc.NitroClient.Interfaces;
+using CoreLayer.Citrix.Adc.NitroData.Api.Configuration.System;
+using CoreLayer.Citrix.Adc.NitroOperations;
 using CoreLayer.Citrix.Adc.NodeBackupWorker.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -24,115 +24,129 @@ namespace CoreLayer.Citrix.Adc.NodeBackupWorker
         private readonly INitroServiceClient _nitroServiceClient;
 
         private readonly System.Timers.Timer _timer;
+        private readonly MetricServer _metricServer;
 
+        private readonly string _outputPath;
+
+        private readonly string[] metricLabelNames;
+        private readonly string[] metricLabels;
+        
         private Counter _successfulBackupCounter;
         private Counter _failedBackupCounter;
         private Gauge _backupProcessDuration;
         private Gauge _backupSize;
-        
 
-        public NodeBackupHostedService(IHostApplicationLifetime hostApplicationLifetime, ILogger<NodeBackupHostedService> logger,
+
+        public NodeBackupHostedService(IHostApplicationLifetime hostApplicationLifetime,
+            ILogger<NodeBackupHostedService> logger,
             IOptions<NodeBackupConfiguration> configuration)
         {
             this._logger = logger;
             _nodeBackupConfiguration = configuration.Value;
-            
-            
+
+            // Configure INitroServiceClient
+            _logger.LogDebug("Setting up NitroServiceClient");
             _nitroServiceClient = new NitroServiceClient(
                 new NitroLoginRequestData(
-                    _nodeBackupConfiguration.Node.Username, 
-                    _nodeBackupConfiguration.Node.Password), 
+                    _nodeBackupConfiguration.Node.Username,
+                    _nodeBackupConfiguration.Node.Password),
                 new NitroServiceConnectionSettings(
                     _nodeBackupConfiguration.Node.NodeAddress,
                     5,
                     NitroServiceConnectionAuthenticationMethod.AutomaticHeaderInjection),
                 _nodeBackupConfiguration.Node.CertificateValidation);
 
-            _timer = new System.Timers.Timer(2000);
+            // Configure timer
+            _logger.LogDebug("Configuring NodeBackup Timer");
+            _timer = new System.Timers.Timer(_nodeBackupConfiguration.Backup.Interval * 1000);
             _timer.Elapsed += TriggerNodeBackup;
-            
+
+            // Configure output
+            _logger.LogDebug("Configuring Output path");
+            _outputPath = _nodeBackupConfiguration.Backup.BasePath;
+            if (_nodeBackupConfiguration.Backup.CreateSubdirectoryForNode)
+                _outputPath = Path.Combine(_nodeBackupConfiguration.Backup.BasePath,
+                    _nodeBackupConfiguration.Node.NodeName);
+
+            // Configure metrics
+            _logger.LogDebug("Configuring Prometheus Metric Labels");
+            metricLabelNames = new[]
+            {
+                "owner",
+                "environment",
+                "node"
+            };
+            metricLabels = new[]
+            {
+                _nodeBackupConfiguration.Node.OwnerName,
+                _nodeBackupConfiguration.Node.EnvironmentName,
+                _nodeBackupConfiguration.Node.NodeName
+            };
+            ConfigureMetrics();
+
+            // Validate configuration
             if (!ConfigurationIsValid(new CancellationToken()).Result)
                 hostApplicationLifetime.StopApplication();
-            
-            if (_nodeBackupConfiguration.Prometheus.Enabled)
-                ConfigureMetrics();
+
+            // Start embedded Prometheus MetricsServer
+            if (!_nodeBackupConfiguration.Prometheus.EnableMetricsServer) return;
+            _logger.LogInformation("Starting metrics server");
+            _metricServer = new MetricServer(hostname: "localhost", port: 5001, useHttps: false);
+            _metricServer.Start();
         }
 
         private void ConfigureMetrics()
         {
+            _logger.LogDebug("Configuring Prometheus Metrics");
+
             _successfulBackupCounter = Metrics.CreateCounter(
                 _nodeBackupConfiguration.Prometheus.NamePrefix + "_adc_nodebackup_success",
                 "Successful backups",
                 new CounterConfiguration
                 {
-                    LabelNames = new[]
-                    {
-                        "owner",
-                        "environment",
-                        "node"
-                    }
+                    LabelNames = metricLabelNames
                 });
-            
+
             _failedBackupCounter = Metrics.CreateCounter(
                 _nodeBackupConfiguration.Prometheus.NamePrefix + "_adc_nodebackup_failure",
                 "Failed backups",
                 new CounterConfiguration
                 {
-                    LabelNames = new[]
-                    {
-                        "owner",
-                        "environment",
-                        "node"
-                    },
+                    LabelNames = metricLabelNames
                 });
-            
+
             _backupProcessDuration = Metrics.CreateGauge(
                 _nodeBackupConfiguration.Prometheus.NamePrefix + "_adc_nodebackup_processingtime",
                 "Failed backups",
                 new GaugeConfiguration
                 {
-                    LabelNames = new[]
-                    {
-                        "owner",
-                        "environment",
-                        "node"
-                    },
+                    LabelNames = metricLabelNames
                 });
-            
+
             _backupSize = Metrics.CreateGauge(
                 _nodeBackupConfiguration.Prometheus.NamePrefix + "_adc_nodebackup_size",
                 "Failed backups",
                 new GaugeConfiguration
                 {
-                    LabelNames = new[]
-                    {
-                        "owner",
-                        "environment",
-                        "node"
-                    },
+                    LabelNames = metricLabelNames
                 });
-        }
-        
-        private void TriggerNodeBackup(Object source, ElapsedEventArgs e)
-        {
-            _logger.LogCritical("Backup was triggered at {0:HH:mm:ss}",
-                e.SignalTime);
-            _successfulBackupCounter.WithLabels(new []
-            {
-                _nodeBackupConfiguration.Node.OwnerName,
-                _nodeBackupConfiguration.Node.EnvironmentName,
-                _nodeBackupConfiguration.Node.NodeName
-            }).Inc();
         }
 
         private async Task<bool> ConfigurationIsValid(CancellationToken stoppingToken)
         {
             _logger.LogInformation("Validating configuration");
+
+            if (_nodeBackupConfiguration.Backup.Interval % 300 != 0)
+            {
+                _logger.LogCritical("Backup interval {0} is invalid, must be factor of 300 (5 minutes)", _nodeBackupConfiguration.Backup.Interval);
+                return false;
+            }
             
-            if (!await CanListBackupOnNode()) return false;
-            // if (!IsValidStartHour()) return false;
-            // if (!IsValidInterval()) return false;
             if (!CanWriteToOutputPath()) return false;
+            if (!await CanListBackupOnNode()) return false;
+            
+            _logger.LogInformation("Executing startup backup to test permissions and connectivity");
+            if (!await ExecuteBackupProcedure(DateTime.Now)) return false;
             
             _logger.LogInformation("Configuration is valid, starting service");
             
@@ -143,12 +157,11 @@ namespace CoreLayer.Citrix.Adc.NodeBackupWorker
         {
             try
             {
-                var result = await ListBackupOnNode();
-                if (!result.ErrorCode.Equals(0))
-                {
-                    _logger.LogCritical(result.Message);
-                    return false;
-                }
+                var listBackupResponse = await NitroBackup.GetAllAsync(_nitroServiceClient);
+                if (listBackupResponse.ErrorCode.Equals(0)) return true;
+                _logger.LogCritical(listBackupResponse.Message);
+                return false;
+
             }
             catch (HttpRequestException ex)
             {
@@ -157,24 +170,13 @@ namespace CoreLayer.Citrix.Adc.NodeBackupWorker
                     _logger.LogCritical(ex.InnerException.Message);
                 return false;
             }
-
-            return true;
         }
-        
-        private async Task<SystemBackupGetResponse> ListBackupOnNode()
-        {
-            var systemBackupGetCommand = NitroCommandFactory.Create<SystemBackupGetCommand>(
-                _nitroServiceClient);
 
-            var result = await systemBackupGetCommand.GetResponse();
-            return result;
-        }
-        
         private bool CanWriteToOutputPath()
         {
             try
             {
-                Directory.CreateDirectory(_nodeBackupConfiguration.Backup.BasePath);
+                Directory.CreateDirectory(_outputPath);
             }
             catch (UnauthorizedAccessException ex)
             {
@@ -217,26 +219,157 @@ namespace CoreLayer.Citrix.Adc.NodeBackupWorker
 
         protected override async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            await Task.Delay(1000, cancellationToken);
+            _logger.LogDebug("Waiting 5 seconds before starting countdown");
+            await Task.Delay(5000, cancellationToken);
             
+            await TriggerExecutionTimer(cancellationToken);
+            
+            _logger.LogInformation("Stopping backup timer");
+            _timer.Stop();
+        }
+
+        private async Task TriggerExecutionTimer(CancellationToken cancellationToken)
+        {
             while (!cancellationToken.IsCancellationRequested)
             {
-                if (!_timer.Enabled && (DateTime.Now.Ticks/10000000).Equals(_nodeBackupConfiguration.Backup.Start.Ticks/10000000))
-                {
-                    _logger.LogCritical("Starting timer");
-                    _timer.Enabled = true;
-                }
-                
+                var nextIntervalCountdown = CalculateNextTargetSeconds();
+
                 if (!_timer.Enabled)
                 {
-                    _logger.LogInformation("Counting down to first backup: {0}",
-                        (_nodeBackupConfiguration.Backup.Start - DateTime.Now).ToString(@"hh\:mm\:ss"));
+                    if (nextIntervalCountdown == 300)
+                    {
+                        _logger.LogInformation("Starting backup timer");
+                        _timer.Enabled = true;
+                        await ExecuteBackupProcedure(DateTime.Now);
+                    }
+                    else
+                    {
+                        _logger.LogInformation("Counting down to backup timer start: {0}",
+                            nextIntervalCountdown.ToString());
+                    }
                 }
-
+                else
+                {
+                    _logger.LogDebug("Counting down to next backup: {0}", nextIntervalCountdown.ToString());
+                }
                 await Task.Delay(1000, cancellationToken);
             }
-            _timer.Stop();
-            _logger.LogInformation("Stopping Execution");
+        }
+
+        private long CalculateNextTargetSeconds()
+        {
+            long nextTargetSeconds;
+            
+            if (DateTime.Now.Ticks / TimeSpan.TicksPerSecond < _nodeBackupConfiguration.Backup.Start.Ticks / TimeSpan.TicksPerSecond)
+            {
+                _logger.LogDebug("Current time is before target start time");
+                nextTargetSeconds = ((_nodeBackupConfiguration.Backup.Start.Ticks - DateTime.Now.Ticks) /
+                                     TimeSpan.TicksPerSecond);
+                
+                if (nextTargetSeconds > _nodeBackupConfiguration.Backup.Interval)
+                {
+                    nextTargetSeconds -= _nodeBackupConfiguration.Backup.Interval;
+
+                    _logger.LogDebug(
+                        "Seconds to next interval : {0}",
+                        nextTargetSeconds.ToString());
+                }
+
+                nextTargetSeconds++;
+            }
+            else
+            {
+                _logger.LogDebug("Current time is later than target start time");
+                nextTargetSeconds = ((DateTime.Now.Ticks - _nodeBackupConfiguration.Backup.Start.Ticks) /
+                                     TimeSpan.TicksPerSecond);
+
+                if (nextTargetSeconds < 0)
+                    nextTargetSeconds = 300 - nextTargetSeconds;
+                else
+                {
+                    nextTargetSeconds = _nodeBackupConfiguration.Backup.Interval - nextTargetSeconds % _nodeBackupConfiguration.Backup.Interval;
+                }
+                _logger.LogDebug(
+                    "Seconds to next interval : {0}",
+                    nextTargetSeconds.ToString());
+            }
+            
+            return nextTargetSeconds;
+        }
+
+        private async void TriggerNodeBackup(Object source, ElapsedEventArgs e)
+        {
+            _logger.LogInformation("Backup was triggered at {0:HH:mm:ss}",
+                e.SignalTime);
+            
+            await ExecuteBackupProcedure(e.SignalTime);
+        }
+
+        private async Task<bool> ExecuteBackupProcedure(DateTime time)
+        {
+            var filename = _nodeBackupConfiguration.Node.NodeName + "_" + time.ToString("yyyyMMdd_HHmmss") + ".tgz";
+
+            if (!await CreateBackup(filename)) return false;
+            if (!await DownloadBackup(filename)) return false;
+
+            var backupDeleteResponse = await NitroBackup.DeleteAsync(_nitroServiceClient, filename);
+            IncreaseSuccessfulBackupCounter();
+            return true;
+        }
+
+        private async Task<bool> CreateBackup(string filename)
+        {
+            var backupCreateResponse = await NitroBackup.CreateAsync(_nitroServiceClient, filename);
+
+            if (backupCreateResponse.ErrorCode.Equals(0)) return true;
+            
+            IncreaseFailedBackupCounter();
+            _logger.LogWarning("Backup failed - " + backupCreateResponse.Message);
+            return false;
+
+        }
+
+        private async Task<bool> DownloadBackup(string filename)
+        {
+            var backupFile = await NitroBackup.DownloadAsBase64Async(_nitroServiceClient, filename);
+
+            return await StoreBackupToDisk(backupFile);
+        }
+
+        private async Task<bool> StoreBackupToDisk(SystemFileConfiguration fileConfiguration)
+        {
+            try
+            {
+                Directory.CreateDirectory(_outputPath);
+
+                await File.WriteAllBytesAsync(_outputPath + "/" + fileConfiguration.FileName,
+                    Convert.FromBase64String(fileConfiguration.FileContent));
+
+                SetBackupSizeGauge(Double.Parse(fileConfiguration.FileSize));
+                _logger.LogInformation("Stored backup {0} in {1}", fileConfiguration.FileName, _outputPath);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        private void IncreaseSuccessfulBackupCounter()
+        {
+            _successfulBackupCounter.WithLabels(metricLabels).Inc();
+        }
+
+        private void IncreaseFailedBackupCounter()
+        {
+            _failedBackupCounter.WithLabels(metricLabels).Inc();
+        }
+
+        private void SetBackupSizeGauge(double size)
+        {
+            _backupSize.WithLabels(metricLabels).IncTo(size);
         }
     }
 }
